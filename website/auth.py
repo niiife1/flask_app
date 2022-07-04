01c232e0301cef5fb51807c30e696c522e745b78
niiife1 <Ivandemirov28@gmail.com>
2022-07-05 00:11:16 +0300

    name fill

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,35 @@
-from flask import Blueprint, render_template
+from unicodedata import category
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=["GET", "POST"])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", text="testing", boolean=True)
-
+    
 @auth.route('/logout')
 def logout():
     return("<p>Logout</p>")
 
-@auth.route('/sing-up')
-def sing_up():
-    return render_template("sing_up.html")
+@auth.route('/sign-up', methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        first_name = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+    
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+           flash('First name  must be greater than 1 characters.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        elif password1 != password2:
+            flash("password don`t match.", category='error')
+        else:
+            flash('Account created!', category="success")
+    return render_template("sign_up.html")
