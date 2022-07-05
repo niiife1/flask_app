d8d21e23da4fc385acc31aa9fe0ca53344d2004c
niiife1 <Ivandemirov28@gmail.com>
2022-07-05 11:45:19 +0300

    work data

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os  import path
 db = SQLAlchemy
-DB_NAME = "database.bd"
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
